fb712a16186ed401b6b993457d7773098fb8797e
Brian Ruff <briananthonyruff@gmail.com>
2021-02-04 20:46:24 -0500

    req for posting new score made

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const env = require("dotenv");
 env.config();
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+express.urlencoded({extended: true});
+
 const db = require("./dbCongig").pool;
 
 
 // db.query("insert into score(score, username) values(-500, 'brian')").then(r1 =>  console.log(r1));
 
 app.post("/", (req, res) => {
-  const username = req.params.score;
-  const score = req.params.username;
-  db.query(`insert into score(score, username) values ${score}, ${username}`).then(r1 => {
-    console.log(r1.rows);
-  }).catch(err => {
-    res.json(err);
-    console.log(err);
-    throw new Error(err);
+  const username = req.body.username;
+  const score = req.body.score;
+  db.query(`insert into score(username, score) values($1, $2) returning *`, [username, score], (err, response) => {
+    if (err) {
+      res.json(err);
+    } else {
+      return res.json(response.rows[0])
+    }
   })
-})
+});
 
 app.get("/", (req, res) => {
   db.query("select * from score").then(r => {
