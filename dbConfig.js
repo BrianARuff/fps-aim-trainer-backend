728973ae78e1f067d77e462fc1953d621d4a2a59
Brian Ruff <briananthonyruff@gmail.com>
2021-02-04 22:28:35 -0500

    change con str

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,4 @@
 const { Client } = require('pg')
-const client = new Client({connectionString: "postgres://npilrnjuhmdmth:3223569ad5677f42568f78ba4566dcaa19c75a6b733ae15b418f8cddb1e20498@ec2-3-214-3-162.compute-1.amazonaws.com:5432/d5pdd1laepqj1f"
-});
+const client = new Client({connectionString: process.env.DATABASE_URL});
 
 exports.client = client;
