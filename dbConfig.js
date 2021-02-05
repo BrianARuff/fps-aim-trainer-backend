7c406f920341978f4d1675385fafde3f156e67b2
Brian Ruff <briananthonyruff@gmail.com>
2021-02-04 21:37:34 -0500

    update for pool con string

--- dbConfig.js
+++ dbConfig.js
@@ -1,16 +1,4 @@
 const { Pool, Client } = require('pg')
-// pools will use environment variables
-// for connection information
-
-
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "fpshighscore",
-//   password: process.env.PSW,
-//   port: 5432
-// });
-
-const pool = new Pool(process.env.DATABASE_URL);
+const pool = new Pool(process.env.DATABASE_URL || "postgres://postgres:1366@localhost:5432/fpshighscore");
 
 exports.pool = pool;
