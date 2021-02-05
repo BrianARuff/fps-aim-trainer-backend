bce86c331e963a815ab43c76267442d7356894e2
Brian Ruff <briananthonyruff@gmail.com>
2021-02-04 23:57:20 -0500

    asdf

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,5 @@
 const env = require("dotenv");
 env.config();
-const { Pool } = require('pg')
+const { Pool } = require('pg').native;
 const pool = new Pool({connectionString: process.env.DATABASE_URL});
 exports.pool = pool;
