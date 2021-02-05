2daa18bfaffe9a450a6326f930d5d373bc7de52d
Brian Ruff <briananthonyruff@gmail.com>
2021-02-05 00:06:33 -0500

    auth

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,8 @@
 const env = require("dotenv");
 env.config();
 const {Pool} = require("pg");
-const pool = new Pool({connectionString: process.env.DATABASE_URL, ssl: true});
+const pool = new Pool({connectionString: process.env.DATABASE_URL, ssl: {
+  rejectUnauthorized: false,
+}});
 pool.connect();
 exports.pool = pool;
