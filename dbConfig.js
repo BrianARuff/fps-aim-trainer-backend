c3080efa7e543c07b505870f3f3997d54845f0e0
Brian Ruff <briananthonyruff@gmail.com>
2021-02-04 22:11:55 -0500

    o

--- dbConfig.js
+++ dbConfig.js
@@ -1,10 +1,11 @@
 const { Client } = require('pg')
-const client = new Client(process.env.DATABASE_URL || {
-  host: 'localhost',
+const client = new Client({
+  host: 'ec2-18-204-101-137.compute-1.amazonaws.com',
   port: 5432,
-  user: 'postgres',
-  password: process.env.PSW,
-  database: "fpshighscore"
+  user: 'rlcvhlskrpmcfh',
+  password: "37feb2728c7b45601edc48344656c2a74c6bbb91572c58a57a8a176ce1a01c0a",
+  database: "d2vvjqce23rq7o",
+  ssl: true
 });
 
 exports.client = client;
